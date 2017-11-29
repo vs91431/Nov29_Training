bbb0d5fb832ca3fc520a5bafee73eabe91714907
vs39 <vinothkumar.a.s@capgemini.com>
2017-11-29 11:03:55 +0530

    branch

--- mvn_demo2/src/main/java/com/capgemini/mvn_demo2/Gitdemo.java
+++ mvn_demo2/src/main/java/com/capgemini/mvn_demo2/Gitdemo.java
@@ -5,7 +5,7 @@ public class Gitdemo {
 	public static void main(String[] args) {
 		System.out.println("Demo2 Git");
 		System.out.println("Checkout");
-
+		System.out.println("Merge branch");
 
 	}
 
