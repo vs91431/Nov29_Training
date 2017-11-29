8426a575f5b0930698e41ab4b12de85927d80949
vs39 <vinothkumar.a.s@capgemini.com>
2017-11-29 10:27:45 +0530

    Git Demo

---
+++ mvn_demo2/src/main/java/com/capgemini/mvn_demo2/Gitdemo.java
@@ -0,0 +1,11 @@
+package com.capgemini.mvn_demo2;
+
+public class Gitdemo {
+
+	public static void main(String[] args) {
+		System.out.println("Demo2 Git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
